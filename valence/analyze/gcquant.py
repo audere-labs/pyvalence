20244bd85a2ad35ec0bf4ff7b5661f48c41fe5b2
Blake Boswell <j.blake.boswell@gmail.com>
2017-07-16 04:32:55 -0400

    create branch to deal with issue #13
    add print statement to highlight where issue occurs

--- valence/analyze/gcquant.py
+++ valence/analyze/gcquant.py
@@ -93,10 +93,9 @@ def std_curves(compiled, standards):
         standards_melted.columns = ['library_id', 'key', 'cal_conc']
 
         print(compiled.index)
-
-        compiled = (compiled.assign(key=compiled.index)
-                            .reset_index(drop=True))
-        return (pd.merge(compiled, standards_melted,
+        
+        return (pd.merge(compiled.reset_index(),
+                         standards_melted,
                          how='left',
                          on=['library_id', 'key'])
                   .dropna(subset=['cal_conc']))
