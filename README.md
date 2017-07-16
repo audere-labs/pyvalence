c9d2de9516208fb3087b5fad92f6db79f04e6962
Blake Boswell <j.blake.boswell@gmail.com>
2017-07-15 22:20:57 -0400

    refactor dir layout, add setup.py

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-## `chemtbd` 
+## `valence` 
 
-Currently under development.  For example of existing functionality see the [example notebook](https://github.com/blakeboswell/chemtbd/blob/master/example.ipynb).
---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+from valence import __version__
+
+
+PKG_NAME = 'valence'
+PKG_AUTHOR = 'Audere Labs'
+PKG_LICENSE = 'BSD'
+
+AUTHOR_EMAIL = 'audere.labs@gmail.com'
+MAINTAINER_EMAIL = '' # google group / forum
+
+URL = 'https://audere.github.com/valence'
+DOWNLOAD_URL = ''
+
+DESCRIPTION = '< 10 word'
+LONG_DESCRIPTION = '''
+    xxx ...
+'''
+
+
+options = {
+    'version': __version__,
+    'name': PKG_NAME,
+    'author': PKG_AUTHOR,
+    'license': PKG_LICENSE,
+    'author_email': AUTHOR_EMAIL,
+    'maintainer_email': MAINTAINER_EMAIL,
+    'url': URL,
+    'download_url': DOWNLOAD_URL,
+    'description': DESCRIPTION,
+    'long_description': LONG_DESCRIPTION,
+    'platforms': ['Any'],
+    'classifiers': [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Chemistry'
+    ],
+    'packages': [
+        'valence', 'valence.build', 'valence.analyze'
+    ],
+    'package_data': {'valence': []},
+    'include_package_data': True,
+    'install_requires': ['pandas', 'numpy', 'scipy']
+}
+
+setup(**options)
